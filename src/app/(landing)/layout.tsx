d49c5aec62f882e2b43bd976264755df87ada2f8
tomtomdu73 <thomas.cosialls@gmail.com>
2024-06-28 15:13:19 +0200

    feat: install google analytics

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,8 +1,8 @@
 import { type Metadata } from 'next'
 
-import { RootLayout } from '@/components/RootLayout'
-
 import '@/styles/tailwind.css'
+import { RootLayout } from '@/components/RootLayout'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 export const metadata: Metadata = {
   title: {
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+      <GoogleAnalytics />
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
