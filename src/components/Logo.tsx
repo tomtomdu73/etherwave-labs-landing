596ae3122b9c4a81f8354332ee5d117be9652d90
tomtomdu73 <thomas.cosialls@gmail.com>
2024-07-09 12:41:39 -0500

    style: update logo versions

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Image from 'next/image'
 import clsx from 'clsx'
 
-import favicon from '@/app/icon.svg'
+import logo from '@/assets/logo.svg'
+import logoLight from '@/assets/logo-light.svg'
 
 export default function Logo({ invert = false }: { invert?: boolean }) {
   return (
     <div className="flex items-center gap-x-2">
-      <Image
-        className={invert ? 'rounded-full bg-white p-1' : ''}
-        src={favicon}
-        alt="logo"
-        width={32}
-        height={32}
-      />
+      {invert ? (
+        <Image src={invert ? logoLight : logo} alt="logo etherwavelabs" width={40} height={40} />
+      ) : (
+        <Image src={logo} alt="logo light etherwavelabs" width={40} height={40} />
+      )}
       <span
         className={clsx(invert && 'text-white', 'hidden font-display text-lg font-medium sm:block')}
       >
