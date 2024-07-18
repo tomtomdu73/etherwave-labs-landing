08a6f602f2c4d6d845c827f0ed204712dd77984f
tomtomdu73 <thomas.cosialls@gmail.com>
2024-07-18 17:43:00 -0500

    feat(contact): add glare effect to contact box

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -2,33 +2,36 @@ import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
 import { Offices } from '@/components/Offices'
+import { GlareCard } from './GlareCard'
 
 export function ContactSection() {
   return (
     <Container className="mt-24 sm:mt-32 lg:mt-40">
-      <FadeIn className="-mx-6 rounded-4xl bg-brand-500 px-6 py-20 sm:mx-0 sm:py-20 md:px-12">
-        <div className="mx-auto max-w-5xl">
-          <div className="flex items-center justify-between gap-10">
-            <div className="flex w-[80%] flex-col gap-4">
-              <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
-                Ready to take your project to the next level?
-              </h2>
-              <p className="mt-6 text-lg text-neutral-200">
-                Contact us today to discuss how we can help you achieve your goals in the blockchain
-                space.
-              </p>
-            </div>
-            <Button href="/contact" invert>
-              Contact us
-            </Button>
+      <GlareCard>
+        <FadeIn className="-mx-6 rounded-4xl bg-brand-500 px-6 py-20 sm:mx-0 sm:py-20 md:px-12">
+          <div className="mx-auto max-w-5xl">
+            <div className="flex items-center justify-between gap-10">
+              <div className="flex w-[80%] flex-col gap-4">
+                <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
+                  Ready to take your project to the next level?
+                </h2>
+                <p className="mt-6 text-lg text-neutral-200">
+                  Contact us today to discuss how we can help you achieve your goals in the
+                  blockchain space.
+                </p>
+              </div>
+              <Button href="/contact" className="z-50" invert>
+                Get in touch
+              </Button>
 
-            {/* <div className="mt-10 border-t border-white/10 pt-10">
+              {/* <div className="mt-10 border-t border-white/10 pt-10">
               <h3 className="font-display text-base font-semibold text-white">Our offices</h3>
               <Offices invert className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2" />
             </div> */}
+            </div>
           </div>
-        </div>
-      </FadeIn>
+        </FadeIn>
+      </GlareCard>
     </Container>
   )
 }
--- src/components/StatList.tsx
+++ src/components/StatList.tsx
@@ -16,19 +16,11 @@ export function StatList({
   )
 }
 
-export function StatListItem({
-  label,
-  value,
-}: {
-  label: string
-  value: string
-}) {
+export function StatListItem({ label, value }: { label: string; value: string }) {
   return (
     <Border as={FadeIn} position="left" className="flex flex-col-reverse pl-8">
-      <dt className="mt-2 text-base text-neutral-600">{label}</dt>
-      <dd className="font-display text-3xl font-semibold text-neutral-950 sm:text-4xl">
-        {value}
-      </dd>
+      <dt className="mt-2 text-lg text-neutral-600">{label}</dt>
+      <dd className="font-display text-3xl font-semibold text-neutral-950 sm:text-4xl">{value}</dd>
     </Border>
   )
 }
