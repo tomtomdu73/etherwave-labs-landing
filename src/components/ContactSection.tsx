3ca45657e16b11961bf8837dee9ea321c075e436
tomtomdu73 <thomas.cosialls@gmail.com>
2024-07-18 22:57:16 -0500

    fix: responsiveness of contact section component

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -6,11 +6,11 @@ import { GlareCard } from './GlareCard'
 
 export function ContactSection() {
   return (
-    <Container className="mt-24 sm:mt-32 lg:mt-40">
+    <Container className="mb-72 mt-24 sm:mt-32 md:mb-48 lg:mb-0 lg:mt-40">
       <GlareCard>
         <FadeIn className="-mx-6 rounded-4xl bg-brand-500 px-6 py-20 sm:mx-0 sm:py-20 md:px-12">
           <div className="mx-auto max-w-5xl">
-            <div className="flex items-center justify-between gap-10">
+            <div className="flex flex-col items-center justify-between gap-10 sm:flex-row">
               <div className="flex w-[80%] flex-col gap-4">
                 <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
                   Ready to take your project to the next level?
